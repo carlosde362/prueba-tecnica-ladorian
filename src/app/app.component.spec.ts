4b2617c358f537e245cb5d4ec661b191f728c7aa
Carlos Martinez <carlosde362@gmail.com>
2023-12-01 22:16:47 +0100

    Fix test app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {TranslateService} from "@ngx-translate/core";
 
 describe('AppComponent', () => {
+  let translateServiceSpy: jasmine.SpyObj<TranslateService>;
   beforeEach(async () => {
+    translateServiceSpy = jasmine.createSpyObj(TranslateService,  ['setDefaultLang', 'getBrowserLang', 'use']);
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        { provide: TranslateService, useValue: translateServiceSpy }
+      ]
     }).compileComponents();
   });
 
@@ -12,18 +18,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'prueba-tecnica-ladorian' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('prueba-tecnica-ladorian');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, prueba-tecnica-ladorian');
   });
 });
