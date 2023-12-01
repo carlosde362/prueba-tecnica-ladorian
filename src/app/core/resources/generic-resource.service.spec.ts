b5ff7aac473e73931d7e81f5673e1a2c97497742
Carlos Martinez <carlosde362@gmail.com>
2023-12-01 22:17:21 +0100

    Fix test and testing request

--- src/app/core/resources/generic-resource.service.spec.ts
+++ src/app/core/resources/generic-resource.service.spec.ts
@@ -1,13 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GenericResourceService } from './generic-resource.service';
+import {GenericResourceService, HttpMethod} from './generic-resource.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 describe('GenericResourceService', () => {
   let service: GenericResourceService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [GenericResourceService],
+    });
     service = TestBed.inject(GenericResourceService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should handle error in request', () => {
+    const url = 'https://mock.com/api/data';
+    const method: HttpMethod = 'GET';
+
+    service.serverRequest(method, url).subscribe({
+      next: () => fail(),
+      error: (error) => {
+        expect(error).toContain('Error');
+      }}
+    );
   });
 
   it('should be created', () => {
